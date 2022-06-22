6b3733e503a875a2de0ec2534ecd50b466bd1b3a
Mac <mac@Macs-MacBook-Pro.local>
2022-06-22 05:08:32 -0700

    changes

--- functions_practice.py
+++ functions_practice.py
@@ -1,23 +1,7 @@
 def hello():
-    print("hello!")
+  print("hello, Mae")
 
-def pack(a,b,c):
-    return [a,b,c]
+def pack(cat, dog, mouse):
+  return [cat, dog, mouse]
+print(pack("cat", "dog", "mouse"))
 
-
-def eat_lunch(my_lst):
-  if len(my_lst) == 0:
-    print("My lunchbox is empty!")
-  else:
-    for i in range(len(my_lst)):
-      if i == 0:
-        print(f"First I eat {my_lst[0]}")
-      else:
-        print(f"Next I eat {my_lst[i]}")
-
-
-hello()
-print(pack("a","b","c"))
-print(pack(1,2,3))
-eat_lunch(["sushi"])
-eat_lunch(["tacos", "salad","seafod", "sushi"])
